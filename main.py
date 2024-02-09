33122ff6d1ba60865463a3e0fbc363e603467232
Nikita <113472417+Xwnx5@users.noreply.github.com>
2024-02-09 11:33:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ app.include_router(
 
 current_user = fastapi_users.current_user()
 
-#@app.get("/protected-route")
-#def protected_route(user: User = Depends(current_user)):
-#    return f"Hello, {user.username}"
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
 
 
 @app.get("/unprotected-route")
